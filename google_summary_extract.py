7abcd3c1c3d09328dd1da84a712dd4d5348e3ef7
sherry725 <shixinwu365@gmail.com>
2021-09-24 11:16:05 +0800

    google_2nd

--- google_summary_extract.py
+++ google_summary_extract.py
@@ -13,7 +13,12 @@ title_result = []
 description_result = []
 keyword_result = []
 
-#webs = ['https://www.healthline.com/health/healthy-sleep/natural-sleep-aids']
+"""
+webs = ['https://www.healthline.com/health/healthy-sleep/natural-sleep-aids',
+        'https://www.mayoclinic.org/healthy-lifestyle/adult-health/in-depth/sleep-aids/art-20047860',
+        'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4394901/']
+"""
+
 for web in webs:
     browser = webdriver.Chrome(options=option, executable_path=chrome_path)
     # cdp - chrome developer protocol
@@ -37,15 +42,6 @@ for web in webs:
         title_result.append(ttl.get_attribute('innerText').strip())
 
     try:
-        dcrip = browser.find_element_by_css_selector('meta[name="description"]')
-    except:
-        print("description not found")
-        description_result.append(" ")
-    else:
-        print(dcrip.get_attribute("content"))
-        description_result.append(dcrip.get_attribute("content").strip())
-
-    try:
         kw = browser.find_element_by_css_selector('meta[name="keywords"]')
     except:
         print("keyword not found")
@@ -54,7 +50,56 @@ for web in webs:
         print(kw.get_attribute("content"))
         keyword_result.append(kw.get_attribute("content").strip())
 
+    try:
+        dcrip1 = browser.find_element_by_css_selector('meta[name="description"]').get_attribute("content").strip()
+    except:
+        dcrip1 = False
+        print("description not work")
+
+    try:
+        dcrip2 = browser.find_element_by_css_selector('meta[name="Description"]').get_attribute("content").strip()
+    except:
+        dcrip2 = False
+        print("Description not work")
+
+    try:
+        dcrip3 = browser.find_element_by_css_selector('meta[property="og:description"]').get_attribute("content").strip()
+    except:
+        dcrip3 = False
+        print("og description not work")
+
+    if dcrip1:
+        description_result.append(dcrip1)
+        print(dcrip1)
+    elif dcrip2:
+        description_result.append(dcrip2)
+        print(dcrip2)
+    elif dcrip3:
+        description_result.append(dcrip3)
+        print(dcrip3)
+    else:
+        print("description not found")
+        description_result.append(" ")
+
     browser.close()
+
+"""        
+    try:
+        dcrip = browser.find_element_by_css_selector('meta[name="description"]')
+    except:
+        try:
+            dcrip = browser.find_element_by_css_selector('meta[name="Description"]')
+        except:
+            print("description not found")
+            description_result.append(" ")
+        else:
+            print(dcrip.get_attribute("content"))
+            description_result.append(dcrip.get_attribute("content").strip())
+    else:
+        print(dcrip.get_attribute("content"))
+        description_result.append(dcrip.get_attribute("content").strip())
+"""
+
 
 links['Title'] = title_result
 links['Description'] = description_result
